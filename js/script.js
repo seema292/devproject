f884b0b2a5db743e9169895a27f76b84fc5c1d22
Seema <seemabhartijsi@gmail.com>
2025-05-21 18:17:56 +0530

    full project

---
+++ js/script.js
@@ -0,0 +1,59 @@
+const navbarMenu = document.getElementById("menu");
+const burgerMenu = document.getElementById("burger");
+const headerMenu = document.getElementById("header");
+const overlayMenu = document.querySelector(".overlay");
+
+// Open Close Navbar Menu on Click Burger
+if (burgerMenu && navbarMenu) {
+  burgerMenu.addEventListener("click", () => {
+    burgerMenu.classList.toggle("is-active");
+    navbarMenu.classList.toggle("is-active");
+  });
+}
+
+// Close Navbar Menu on Click Links
+document.querySelectorAll(".menu-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    burgerMenu.classList.remove("is-active");
+    navbarMenu.classList.remove("is-active");
+  });
+});
+
+// Fixed Navbar Menu on Window Resize
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 992) {
+    if (navbarMenu.classList.contains("is-active")) {
+      navbarMenu.classList.remove("is-active");
+      overlayMenu.classList.remove("is-active");
+    }
+  }
+});
+
+// Dark and Light Mode on Switch Click
+document.addEventListener("DOMContentLoaded", () => {
+  const darkSwitch = document.getElementById("switch");
+
+  darkSwitch.addEventListener("click", () => {
+    document.documentElement.classList.toggle("darkmode");
+    document.body.classList.toggle("darkmode");
+  });
+});
+
+// faq
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute("aria-expanded");
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute("aria-expanded", "false");
+  }
+
+  if (itemToggle == "false") {
+    this.setAttribute("aria-expanded", "true");
+  }
+}
+
+items.forEach((item) => item.addEventListener("click", toggleAccordion));
+
+// faq
